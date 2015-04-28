54eb9d6049e0090879d059638da0e6ab0d033c78
hugoderiii <hugoderiii@gmail.com>
2015-04-28 12:54:18 +0200

    adding a post request
    Eine Post Anfrage an den Application Server, um die gewünschten Daten zu erhalten

--- webserver/webserver.js
+++ webserver/webserver.js
@@ -16,33 +16,59 @@ app.use(express.static(path.resolve(__dirname +'/../PVCWebsite')));
 
 
 app.get('/data', function (req, res) {
-
+	
 	console.log("sensortype: " + req.query.sensortype);	
 	console.log("areas: " + req.query.areas);
 	console.log("aggregation: " + req.query.aggregation);
 	console.log("startdate: " + req.query.startdate);
 	
-	var request = require('request');
-
-request.post(
-    'http://www.yoursite.com/formpage',
-    { form: { key: 'value' } },
-    function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(body)
-        }
-    }
-);
+	var finerAggregation;
+	var endDate = new Date();
+	var startDate = new Date(req.query.startdate);
+	var areas = req.query.areas;
+	var sensortype req.query.sensortype;
 	
-	http.get ( {
-		area : "xy",
-		sensortype : "xy",
-		startDate : "xy",
-		endDate : "xy",
-		aggregation : "xy"
-		
-	})
+	switch(req.query.aggregation) {
+    	case "year":
+        	finerAggreation = "month";
+        	endDate.setDate(startDate.getMonth() + 12); 
+        	break;
+    	case "month":
+        	finerAggreation = "day";
+        	endDate.setDate(startDate.getMonth() + 1);
+        	break;
+        case "week":
+        	finerAggreation = "day";
+        	endDate.setDate(startDate.getDate() + 7);
+        	break;
+        case "day":
+        	finerAggreation = "hour";
+        	endDate = startDate;
+        	break;
+	}
+ 
+	var request = require('request');
+	request({
+    		url: '../datastation/myapp', //URL to hit
+    		//qs: {from: 'blog example', time: +new Date()}, //Query string data
+    		method: 'POST',
+    		//Lets post the following key/values as form
+    		json: {
+        		areas : areas,
+			sensortype : sensortype,
+			startDate : startDate,
+			endDate : endDate,
+			aggregation : finerAggregation
+    		}
+	}, function(error, response, body){
+    		if(error) {
+        		console.log(error);
+    		} else {
+        		console.log(response.statusCode, body);
+		}
+	});
 
+	
 	res.json(jsonfile);
 });
 
