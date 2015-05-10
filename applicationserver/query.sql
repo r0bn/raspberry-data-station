68c10edb481215d3e3a23be88b966e3c7d610ec6
D1rk2 <D1rk2@users.noreply.github.com>
2015-05-10 21:10:39 +0200

    Modified /query endpoint

--- applicationserver/query.sql
+++ applicationserver/query.sql
@@ -1,11 +1,11 @@
-SELECT D.Area, avg(M.Value) AS Average, min(M.Value)AS Minimum, max(M.Value) AS Maximum, strftime("%Y-%m-%d %H:%M:%S",M.Timestamp) AS Timestamp
+SELECT D.ID, avg(M.Value) AS Average, min(M.Value)AS Minimum, max(M.Value) AS Maximum, strftime("%Y-%m-%d %H:%M:%S",M.Timestamp) AS Timestamp
 FROM Data AS M 
 JOIN Sensors AS SE ON SE.ID = M.SensorID 
 JOIN Datastations AS D ON SE.DatastationID = D.ID 
 JOIN Sensortype AS ST ON SE.SensortypeID = ST.ID
 WHERE strftime("%Y-%m-%d %H:%M%S", M.Timestamp) 
-BETWEEN "2015-05-06 00:00:00"
-AND "2015-05-07 00:00:00"
-AND D.Area = "1/111"
-AND ST.Name = "temperature"
-GROUP BY D.Area, strftime("%d", M.Timestamp)
+BETWEEN "2015-03-31 00:00:00"
+AND "2015-04-09 00:00:00"
+AND D.ID = "1" OR D.ID = "2" 
+AND ST.ID = "1"
+GROUP BY D.ID, strftime("%d", M.Timestamp)
