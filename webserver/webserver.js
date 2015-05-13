61b9c205fc7f15a03c8bb9240bd9c0eaaeb584ee
hugoderiii <hugoderiii@gmail.com>
2015-05-13 13:18:39 +0200

    implemented timespan comparison support + refactor

--- webserver/webserver.js
+++ webserver/webserver.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var request = require('request');
-var requestSync = require("sync-request");
+var async = require('async');
 var app = express();
 var jsonfile= require(__dirname + "/dummydata.json");
 var dataFile= require(__dirname + "/dataFile.json");
@@ -19,8 +19,6 @@ var server = app.listen(config.development.webserver.port, function () {
 app.use(express.static(path.resolve(__dirname +'/../PVCWebsite')));
 
 app.get('/init', function (req, res) {
-	
-	var request = require('request');
 	var jsonresponse = {};
 	
 	request('http://localhost:'+config.development.applicationserver.port+'/allDatastations', function (error, response, body) {
@@ -38,22 +36,71 @@ app.get('/init', function (req, res) {
 	});
 	}
 	});
+
 });
 
 
 app.get('/data', function (req, res) {
 	console.log("query:" + JSON.stringify(req.query));
+	req.query.datastationID = req.query.datastationID.split(",");
+	var startdates = req.query.startdate.split(",");
+	var dataArray = [];
+	var jsonresponse = {};
+	jsonresponse.request = req.query;
 	
-	var finerAggregation;
+	async.each(startdates,
+		function(startdate, callback){
+			req.query.startdate = startdate;
+			requestData(req, function (data) {
+				dataArray.push(data);
+				callback()
+			});
+	},
+		function(){
+			if (dataArray.length == 1){
+				jsonresponse.data = dataArray[0];
+			} else  if (dataArray.length == 2){
+				jsonresponse.data = mergeData(dataArray);
+			}
+			res.json(jsonresponse);
+		}
+	);
+});
+	
+function mergeData(dataArray)
+{
+	var timeframes1 = dataArray[0].timeframes;
+	var timeframes2 = dataArray[1].timeframes;
+	
+	var size = (timeframes1.length > timeframes2.length) ? timeframes1.length : timeframes2.length;
+	var timeframesMerged = [];
+	for (var i = 0; i< size; i++){
+		var value1 = (i<timeframes1.length) ? timeframes1[i] : " - ";
+		var value2 = (i<timeframes2.length) ? timeframes2[i] : " - ";
+		timeframesMerged[i] = value1 +"/"+ value2;
+	}
+	
+	var dataPerArea1 = dataArray[0].dataPerArea;
+	var dataPerArea2 = dataArray[1].dataPerArea;
+	var dataPerAreaMerged = dataPerArea1.concat(dataPerArea2);
+	
+	var dataMerged = {};
+	dataMerged.title = dataArray[0].title;
+	dataMerged.timeframes = timeframesMerged;
+	dataMerged.dataPerArea = dataPerAreaMerged;
+	return dataMerged;
+}
+	
+var requestData = function(req, callback){
+    var finerAggregation;
 	var dateParts = req.query.startdate.split("/");
 	var str = dateParts[2]+"-"+dateParts[1]+"-"+dateParts[0]+" 00:00:00";
 	var startDate = new Date(str);
 	var endDate = new Date(startDate);
-	req.query.datastationID = req.query.datastationID.split(",");
 	var datastationID = req.query.datastationID;   
 	var sensortypeID = req.query.sensortypeID;
 	sensortypeID = 1;
-	datastationID = [100];
+	datastationID = [1];
 	
 	switch(req.query.timespan) {
     	case "year":
@@ -78,19 +125,14 @@ app.get('/data', function (req, res) {
         	break;
 	}
 	
-	console.log("startDate: " + startDate);
-	console.log("endDate: " + endDate);
-	console.log("finerAggregation: " + finerAggregation);
-	
-	var request = require('request');
 	request({
     	url: 'http://localhost:'+ config.development.applicationserver.port+'/query', //URL to hit
     	method: 'POST',
     	json: {
         	DatastationID : datastationID,
 			SensortypeID : sensortypeID,
-			StartDate : formatDate(startDate),
-			EndDate : formatDate(endDate),
+			StartDate : startDate,
+			EndDate : endDate,
 			Aggregation : finerAggregation
     	}
 	}, function(error, response, body){
@@ -106,12 +148,12 @@ app.get('/data', function (req, res) {
 				var areaDictionary = createAreaDictionary(datastationID, timeframes.length);		
 				areaDictionary = fillAreaDictionary (startDate, finerAggregation, areaDictionary, rows)
 				
-				var jsonresponse = createDataResponse (title, timeframes, areaDictionary, datastationID, req.query);
-				res.json(jsonresponse);
-				test = jsonresponse;
+				var data = createDataResponse (title, timeframes, areaDictionary, datastationID, req.query);
+				callback(data);
 			}
-		});
-	});
+		}
+	);
+};
 
 function formatDate(date)
 {
@@ -133,10 +175,7 @@ function createDataResponse(title, timeframes, areaDictionary, datastationID, qu
 		areasData.data = areaDictionary[item];
 		data.dataPerArea.push(areasData);
 		});
-	var jsonresponse = {};
-	jsonresponse.request = query;
-	jsonresponse.data = data;
-	return jsonresponse;
+	return data;
 }
 	
 function createTimeframes (startDate, endDate, finerAggregation){
@@ -178,21 +217,21 @@ function createAreaDictionary(datastationID, length)
 function fillAreaDictionary (startDate, finerAggregation, areaDictionary, rows)
 {
 	rows.forEach(function(item) {
-	var difference = 0;
-	switch(finerAggregation) {
-		case "m": 
-			difference =  new Date(item.Timestamp).getMonth() - (startDate.getMonth());
-			difference = (difference < 0)?difference + 12 : difference;
-			break;
-		case "d":
-			difference =  new Date(item.Timestamp).getDate() - startDate.getDate();
-			var monthDays = new Date(startDate.getFullYear(), startDate.getMonth()+1, 0).getDate();
-			difference = (difference < 0)?difference + monthDays : difference;
-			break;
-		case "H":
-			difference =  new Date(item.Timestamp).getHours() - startDate.getHours();
-			difference = (difference < 0)?difference + 24 : difference;
-			break;
+		var difference = 0;
+		switch(finerAggregation) {
+			case "m": 
+				difference =  new Date(item.Timestamp).getMonth() - (startDate.getMonth());
+				difference = (difference < 0)?difference + 12 : difference;
+				break;
+			case "d":
+				difference =  new Date(item.Timestamp).getDate() - startDate.getDate();
+				var monthDays = new Date(startDate.getFullYear(), startDate.getMonth()+1, 0).getDate();
+				difference = (difference < 0)?difference + monthDays : difference;
+				break;
+			case "H":
+				difference =  new Date(item.Timestamp).getHours() - startDate.getHours();
+				difference = (difference < 0)?difference + 24 : difference;
+				break;
 		}
 	
 		var dataObject = {};
